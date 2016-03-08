e2822fd9779ea1188423d5ce62479b8cdeb1efe8
jpavlich <jpavlich@javeriana.edu.co>
2016-03-08 16:52:35 -0500

    ...

---
+++ co.edu.javeriana.zoe.generator.persistence/src/temp/Query.java
@@ -0,0 +1,70 @@
+package temp;
+
+public class Query {
+	Query next;
+
+	
+	
+	class Select extends Query {
+		String[] columnas;
+
+		public Select(Query previous, String[] columnas) {
+			super(previous);
+			this.columnas = columnas;
+		}
+
+	}
+
+	class From extends Query {
+		String tabla;
+
+		public From(Query previous, String tabla) {
+			super(previous);
+			this.tabla = tabla;
+		}
+
+	}
+
+	class Where extends Query {
+		public Where(Query previous, String criterio) {
+			super(previous);
+			this.criterio = criterio;
+		}
+
+		String criterio;
+	}
+
+	
+	
+	
+	public Query(Query previous) {
+		super();
+		previous.next = this;
+	}
+	
+	
+	/////////////
+
+	public Query select(String... columnas) {
+		return new Select(this, columnas);
+	}
+
+	public Query from(String tabla) {
+		return new From(this, tabla);
+	}
+
+	public Query where(String criterio) {
+		return new Where(this, criterio);
+	}
+	
+	public String toSQL() {
+		return "";
+	}
+	
+	////////////////
+
+	public static void main(String[] args) {
+		new Query(null).select("almuerzo").from("dieta").where("...");
+
+	}
+}
